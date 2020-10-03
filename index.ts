d50c649911fc2f6724feef9d186f36ef329afcd7
Anwesh43 <anweshthecool0@gmail.com>
2020-10-03 17:28:03 +0530

    adding BCJNode class

--- index.ts
+++ index.ts
@@ -148,3 +148,45 @@ class Animator {
         }
     }
 }
+
+class BCJNode {
+
+    state : State = new State()
+    next : BCJNode 
+    prev : BCJNode 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BCJNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBCJNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BCJNode {
+        var curr : BCJNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
