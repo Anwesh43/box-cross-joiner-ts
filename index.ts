08797d23a31b4a92739a501bdaf93b6bb97731d2
Anwesh43 <anweshthecool0@gmail.com>
2020-10-03 17:01:54 +0530

    DrawingUtil class

--- index.ts
+++ index.ts
@@ -11,6 +11,8 @@ const scGap : number = 0.02 / parts
 const sizeFactor : number = 5.9  
 const delay : number = 20
 const backColor : string = "#bdbdbd"
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
 
 class ScaleUtil {
 
@@ -24,5 +26,50 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBoxCrossJoiner(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const size : number = Math.max(w, h) / sizeFactor 
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const si : number = 1 - 2 * i
+        const y : number = size * 0.5 * si
+        context.save()
+        context.translate(-w / 2, 0)
+        context.fillRect(0, -size / 2, size * sf1, size)
+        DrawingUtil.drawLine(context, size, -y, size + (w / 2 - size) * sf2, -y + size * sf2)
+        DrawingUtil.drawLine(context, size, y, size + (w / 2 - size) * sf3, y + size * sf3)
+        context.restore()
+    }
+
+    static drawBoxCrossJoiners(context : CanvasRenderingContext2D, scale : number) {
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawBoxCrossJoiner(context, j, scale)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBCJNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBoxCrossJoiners(context, scale)
     }
 }
